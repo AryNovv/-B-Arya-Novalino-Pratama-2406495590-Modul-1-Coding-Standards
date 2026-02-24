8a950afd5da5b4088f16f5d9fc39ff75a9dcbac3
AryNov <arya.nov.pratama@gmail.com>
2026-02-24 20:45:28 +0700

    deleted unused imports

--- src/main/java/com/example/eshop/service/ProductServiceImpl.java
+++ src/main/java/com/example/eshop/service/ProductServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.eshop.service;
 
 import com.example.eshop.model.Product;
 import com.example.eshop.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
